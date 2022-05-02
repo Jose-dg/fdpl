b75ac5a654c59bfc6ebc68077dc8641e958b3bb6
joseojeda <jose.ojeda.k@hotmail.com>
2022-05-02 13:45:19 -0500

    Config

--- fdpl/settings.py
+++ fdpl/settings.py
@@ -152,6 +152,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, ''), )
 
 # Default primary key field type
