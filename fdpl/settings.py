15a735fae1794b29b61cd3954c4026468e9e7bf9
joseojeda <jose.ojeda.k@hotmail.com>
2022-05-02 09:51:02 -0500

    Config

--- fdpl/settings.py
+++ fdpl/settings.py
@@ -143,14 +143,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static')
 # )
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, ''), )
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, ''), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
